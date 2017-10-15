aab3caa8abbe63b03bec092095b98492f8c14dff
gbosnjak <bosnjak.gabrijela@gmail.com>
2017-10-15 11:45:15 +0200

    First 10 exercises

---
+++ ex05.rb
@@ -0,0 +1,17 @@
+my_name = 'Gabi'
+my_age = 29
+my_height = 170
+my_weight = 60
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} centimeteres tall."
+puts "She's #{my_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "She's teeth are usually #{my_teeth} depending on the coffee."
+
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
